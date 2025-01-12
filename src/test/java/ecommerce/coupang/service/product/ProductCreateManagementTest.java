60e1af9aabf4ad2dc61449e14c6e0ad2b89afdd2
h0ch1 <h0ch1@naver.com>
2025-01-12 12:57:37 +0900

    productCreateManagement 테스트 작성

--- src/test/java/ecommerce/coupang/service/product/ProductCreateManagementTest.java
+++ src/test/java/ecommerce/coupang/service/product/ProductCreateManagementTest.java
@@ -20,6 +20,9 @@ import ecommerce.coupang.domain.category.CategoryOption;
 import ecommerce.coupang.domain.category.VariantOption;
 import ecommerce.coupang.domain.product.Product;
 import ecommerce.coupang.domain.product.ProductCategoryOption;
+import ecommerce.coupang.domain.product.variant.ProductStatus;
+import ecommerce.coupang.domain.product.variant.ProductVariant;
+import ecommerce.coupang.domain.product.variant.ProductVariantOption;
 import ecommerce.coupang.domain.store.Store;
 import ecommerce.coupang.dto.request.product.CreateProductRequest;
 import ecommerce.coupang.dto.request.product.option.OptionRequest;
@@ -36,20 +39,23 @@ class ProductCreateManagementTest {
 	private ProductCreateManagement productCreateManagement;
 
 	@Mock
-	private ProductVariantOptionService productVariantOptionService;
-
-	@Mock
-	private ProductCategoryOptionService productCategoryOptionService;
-
-	@Mock
 	private CategoryOptionService categoryOptionService;
 
 	@Mock
 	private VariantOptionService variantOptionService;
 
+	@Mock
+	private ProductCategoryOptionService productCategoryOptionService;
+
+	@Mock
+	private ProductVariantOptionService productVariantOptionService;
+
 	private Store mockStore = mock(Store.class);
 	private Category mockCategory = mock(Category.class);
 	private Product mockProduct = mock(Product.class);
+	private ProductVariant mockProductVariant = mock(ProductVariant.class);
+	private CategoryOption mockCategoryOption = mock(CategoryOption.class);
+	private VariantOption mockVariantOption = mock(VariantOption.class);
 
 
 	@Test
@@ -57,22 +63,59 @@ class ProductCreateManagementTest {
 	void createProductAndVariantAndOptions() throws CustomException {
 		CreateProductRequest request = createProductRequest();
 
+		when(categoryOptionService.getCategoryOption(anyLong())).thenReturn(List.of(mockCategoryOption));
+		when(mockCategoryOption.getId()).thenReturn(1L);
+		when(variantOptionService.getVariantOption(anyLong())).thenReturn(List.of(mockVariantOption));
+		when(mockVariantOption.getId()).thenReturn(1L);
+
 		try (MockedStatic<Product> mockedStatic = mockStatic(Product.class)) {
-			when(categoryOptionService.getCategoryOption(anyLong())).thenReturn(List.of(mock(CategoryOption.class)));
-			when(variantOptionService.getVariantOption(anyLong())).thenReturn(List.of(mock(VariantOption.class)));
-			Product mockProduct = mock(Product.class);
-			mockedStatic.when(() -> Product.create(request, mockStore, mockCategory))
+			when(mockProduct.getCategory()).thenReturn(mockCategory);
+			mockedStatic.when(() -> Product.of(request, mockStore, mockCategory))
 				.thenReturn(mockProduct);
 
 			Product product = productCreateManagement.createProductAndVariantAndOptions(request, mockStore, mockCategory);
 			assertThat(product).isNotNull();
 		}
+	}
 
-		// when(Product.create(request, mockStore, mockCategory)).thenReturn(mockProduct);
-		// when(categoryOptionService.getCategoryOption(anyLong())).thenReturn(List.of(mock(CategoryOption.class)));
-		// when(variantOptionService.getVariantOption(anyLong())).thenReturn(List.of(mock(VariantOption.class)));
+	@Test
+	@DisplayName("상품 변형 생성 후 옵션 추가")
+	void createVariantAndOptions() throws CustomException {
+		CreateProductVariantRequest request = createProductVariantRequest();
 
+		when(variantOptionService.getVariantOption(anyLong())).thenReturn(List.of(mockVariantOption));
+		when(mockVariantOption.getId()).thenReturn(1L);
 
+		try (MockedStatic<ProductVariant> mockedStatic = mockStatic(ProductVariant.class)) {
+			when(mockProduct.getCategory()).thenReturn(mockCategory);
+			mockedStatic.when(() -> ProductVariant.of(request, mockProduct))
+				.thenReturn(mockProductVariant);
+
+			ProductVariant productVariant = productCreateManagement.createVariantAndOptions(request, mockProduct);
+			assertThat(productVariant).isNotNull();
+		}
+	}
+
+	@Test
+	@DisplayName("상품 카테고리 옵션 추가")
+	void addCategoryOptionsToProduct() throws CustomException {
+		when(productCategoryOptionService.createProductCategoryOption(1L, mockProduct))
+			.thenReturn(mock(ProductCategoryOption.class));
+
+		productCreateManagement.addCategoryOptionsToProduct(1L, mockProduct);
+
+		verify(mockProduct).addProductOptions(any(ProductCategoryOption.class));
+	}
+
+	@Test
+	@DisplayName("상품 변형 변형 옵션 추가")
+	void addVariantOptionToProductVariant() throws CustomException {
+		when(productVariantOptionService.createProductVariantOption(1L, mockProductVariant))
+			.thenReturn(mock(ProductVariantOption.class));
+
+		productCreateManagement.addVariantOptionToProductVariant(1L, mockProductVariant);
+
+		verify(mockProductVariant).addProductVariantOptions(any(ProductVariantOption.class));
 	}
 
 	private CreateProductRequest createProductRequest() {
@@ -80,8 +123,25 @@ class ProductCreateManagementTest {
 			"name",
 			"description",
 			1L,
-			List.of(mock(OptionRequest.class)),
-			List.of(mock(CreateProductVariantRequest.class))
+			List.of(optionRequest()),
+			List.of(createProductVariantRequest())
+		);
+	}
+
+	private CreateProductVariantRequest createProductVariantRequest() {
+		return new CreateProductVariantRequest(
+			1000,
+			100,
+			ProductStatus.ACTIVE,
+			true,
+			List.of(optionRequest())
+		);
+	}
+
+	private OptionRequest optionRequest() {
+		return new OptionRequest(
+			1L,
+			1L
 		);
 	}
 
