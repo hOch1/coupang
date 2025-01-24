6654f051602ae38ac0e79c335b9340f9bdce81b0
h0ch1 <h0ch1@naver.com>
2025-01-24 10:08:01 +0900

    spring batch 추가 & 월별, 일별 판매량 통계 테이블 추가

---
+++ src/main/java/ecommerce/coupang/domain/store/statistics/MonthSalesStatistics.java
@@ -0,0 +1,41 @@
+package ecommerce.coupang.domain.store.statistics;
+
+import java.time.YearMonth;
+
+import ecommerce.coupang.domain.store.Store;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class MonthSalesStatistics {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "store_id", nullable = false)
+	private Store store;
+
+	@Column(name = "total_sales_count", nullable = false)
+	private int totalSalesCount;
+
+	@Column(name = "total_sales_price", nullable = false)
+	private int totalSalesPrice;
+
+	@Column(name = "date", nullable = false)
+	private YearMonth date;
+}
